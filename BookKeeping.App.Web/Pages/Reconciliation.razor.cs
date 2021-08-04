15dc7ea4af1bc2c7199f8dca647f06fdfd7c07a1
rafsanul Hasan <rafsanulhasan@outlook.com>
2021-08-05 05:08:02 +0600

    Segregated C# code from Razor into a Code behind

---
+++ BookKeeping.App.Web/Pages/Reconciliation.razor.cs
@@ -0,0 +1,44 @@
+﻿using BookKeeping.API.DTOs;
+
+using Microsoft.AspNetCore.Components;
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BookKeeping.App.Web.Pages
+{
+	public partial class Reconciliation
+		: ComponentBase
+	{
+		private int _selectedYear;
+		private ICollection<int> _years;
+		private IncomeExpenseDto _dto;
+		private bool _invalidSelection;
+		private bool _isLoading;
+
+		[Inject]
+		public HttpClient Http { get; set; }
+		private async void OnChange(ChangeEventArgs args)
+		{
+			if (_selectedYear > 0)
+			{
+				_isLoading = true;
+				_invalidSelection = false;
+				_dto = await Http.GetFromJsonAsync<IncomeExpenseDto>($"/api/Transactions/{args.Value}");
+				StateHasChanged();
+			}
+			else
+			{
+				_isLoading = false;
+				_invalidSelection = true;
+			}
+		}
+
+		protected override async Task OnInitializedAsync()
+		{
+			_years = await Http.GetFromJsonAsync<int[]>("/api/Transactions/years");
+		}
+	}
+}
