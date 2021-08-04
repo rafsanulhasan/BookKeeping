ee836c3ad46eac64038f9f78547cb45560a5ea55
rafsanul Hasan <rafsanulhasan@outlook.com>
2021-08-05 04:27:22 +0600

    Fixed entrypoint

--- BookKeeping.API/Program.cs
+++ BookKeeping.API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace BookKeeping.API
 {
-	public class Program
+	public static class Program
 	{
 		public static void Main(string[] args) 
 			=> CreateHostBuilder(args).Build().Run();
