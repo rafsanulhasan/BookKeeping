07af4b87def6ccb3f11690c4451229439ddb0a2c
rafsanul Hasan <rafsanulhasan@outlook.com>
2021-08-07 08:56:10 +0600

    Removed unnecessary files

--- BookKeeping.App.Web/Helpers/HttpStates.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BookKeeping.App.Web.Helpers
-{
-	public class HttpStates
-	{
-		public static readonly IDictionary<string, string> ETags = new Dictionary<string, string>();
-	}
-}
