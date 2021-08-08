f44e43429f2d00729ee35cdd96405a4f264d17a7
rafsanul Hasan <rafsanulhasan@outlook.com>
2021-08-08 16:19:27 +0600

    Revert "Implemented ReactiveUI with Redux"
    This reverts commit 43112c7d8999fc2a660af6e13d1cc0f78a5cf7d8.

--- BookKeeping.App.Web/Program.cs
+++ BookKeeping.App.Web/Program.cs
@@ -1,5 +1,3 @@
-using BookKeeping.App.Web.ViewModels;
-
 using Fluxor;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -35,7 +33,6 @@ namespace BookKeeping.App.Web
 				http.DefaultRequestHeaders.Add(HeaderNames.CacheControl, "public,max-age=60");
 				return http;
 			});
-			services.AddScoped<IncomeExpenseViewModel>();
 
 			services.AddFluxor(c =>
 			{
